1eec56162e31b33ff8c8633abea352cbed441eae
Андрей <parabellum.koval@gmail.com>
2024-09-13 11:22:09 +0300

    viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
 <html>
   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
       @hasSection('meta_title')
         <title>@yield('meta_title')</title>
       @else
