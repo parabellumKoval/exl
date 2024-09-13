6a979135e2689dfdae1903a6fa3aa679e36bf6c1
Ivanio Gamazio <101927589+SuperGamazio@users.noreply.github.com>
2024-09-13 12:11:16 +0300

    Update app.blade.php
    <?php
      //set headers to NOT cache a page
      header("Cache-Control: no-cache, no-store, must-revalidate"); //HTTP 1.1
      header("Pragma: no-cache"); //HTTP 1.0
      header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
    ?>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,9 @@
+<?php
+  //set headers to NOT cache a page
+  header("Cache-Control: no-cache, no-store, must-revalidate"); //HTTP 1.1
+  header("Pragma: no-cache"); //HTTP 1.0
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?>
 <!DOCTYPE html>
 <html>
   <head>
