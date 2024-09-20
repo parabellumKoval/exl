e4da7d62ced51b0a0276766c3b9f8ac754cfeccc
Ivanio Gamazio <101927589+SuperGamazio@users.noreply.github.com>
2024-09-20 13:41:06 +0300

    Update cache.php
    .

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'none'),
+    'default' => env('CACHE_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
