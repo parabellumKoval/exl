f5127d509ca86a72e4082dd68d8a8a7a8da12d43
Ivanio Gamazio <101927589+SuperGamazio@users.noreply.github.com>
2024-09-20 13:39:37 +0300

    Update closed.blade.php
    .

--- resources/views/layouts/closed.blade.php
+++ resources/views/layouts/closed.blade.php
@@ -1,10 +1,3 @@
-@php
-
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-
-@endphp
 <!DOCTYPE html>
 <html>
   <head>
