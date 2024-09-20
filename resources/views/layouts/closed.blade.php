10dab5f1375f48b0e3381bc7ac5cb6f98a7c2ac2
Ivanio Gamazio <101927589+SuperGamazio@users.noreply.github.com>
2024-09-20 12:00:35 +0300

    Update closed.blade.php
    @php
    
    header("Cache-Control: no-cache, no-store, must-revalidate");
    header("Pragma: no-cache");
    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
    
    
    @endphp

--- resources/views/layouts/closed.blade.php
+++ resources/views/layouts/closed.blade.php
@@ -1,10 +1,10 @@
-<?php
-  //set headers to NOT cache a page
-  header("Cache-Control: no-cache, no-store, must-revalidate"); //HTTP 1.1
-  header("Pragma: no-cache"); //HTTP 1.0
-  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-?>
+@php
 
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+@endphp
 <!DOCTYPE html>
 <html>
   <head>
